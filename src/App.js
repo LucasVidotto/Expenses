a1d4599af201eecf512fc81691a79ac41af1c6aa
Lucas Vidotto <lucasvidotto3@gmail.com>
2022-04-10 21:03:40 -0300

    ending without states

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import ExpenseItem from './components/ExpenseItem';
+import Expenses from './components/Expenses/Expenses';
 
 function App() {
   const expenses = [
@@ -28,29 +28,16 @@ function App() {
       date: new Date(2021, 5, 12),
     },
   ];
+  /* return React.createElement(
+    'div',
+    {},
+    React.createElement('h2', {}, "Lets's get started"),
+    React.createElement(expenses, { items: expenses })
+  ); */
   return (
     <div>
       <h2>Lets get started!</h2>
-      <ExpenseItem
-        title={expenses[0].title}
-        amount={expenses[0].amount}
-        date={expenses[0].date}
-      />
-      <ExpenseItem
-        title={expenses[1].title}
-        amount={expenses[1].amount}
-        date={expenses[1].date}
-      />
-      <ExpenseItem
-        title={expenses[2].title}
-        amount={expenses[2].amount}
-        date={expenses[2].date}
-      />
-      <ExpenseItem
-        title={expenses[3].title}
-        amount={expenses[3].amount}
-        date={expenses[3].date}
-      />
+      <Expenses items={expenses} />
     </div>
   );
 }

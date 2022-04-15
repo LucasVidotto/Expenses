17ab01d83be3516da8283dc127ade29e4252e399
Lucas Vidotto <lucasvidotto3@gmail.com>
2022-04-14 21:26:31 -0300

    estados dinamicos aplicados

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -4,11 +4,14 @@ import Card from '../UI/Card';
 import './ExpenseItem.css';
 
 function ExpenseItem(props) {
+  /*   const [title, setTitle] = useState(props.title);
+  
+    console.log('ok'); */
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{ props.title }</h2>
+        <h2>{props.title}</h2>
         <div className='expense-item__price'>
           ${props.amount}
         </div>
